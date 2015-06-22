9059154abb763a84aaf52e1585cdddf6c2cddc33
Cory Walker <cwalker32@gmail.com>
2015-06-22 07:16:29 +0000

    tmux configuration.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 mv ~/.vimrc ~/.vimrc.bak
 ln -s ~/dotfiles/vimrc ~/.vimrc
+mv ~/.tmux.conf ~/.tmux.conf.bak
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 vim +PluginInstall +qall
